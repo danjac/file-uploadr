644db1bc5054c0cd466a240cf79738df1f4b2ba5
Dan Jacob <danjac354@gmail.com>
2011-10-30 16:28:26 +0200

    Starting mongodb branch

--- src/file_uploadr/server.clj
+++ src/file_uploadr/server.clj
@@ -1,9 +1,18 @@
 (ns file-uploadr.server
   (:require [file-uploadr.middleware :as middleware]
-            [noir.server :as server]))
+            [file-uploadr.config :as conf]
+            [noir.server :as server])
+  (:use somnium.congomongo))
+
 
 (server/add-middleware middleware/authenticate)
 (server/load-views "src/file_uploadr/views/")
+
+(def conn (make-connection (:name conf/db)
+                           {:host (or (:host conf/db) "127.0.0.1")}))
+
+(set-connection! conn)
+
 
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
